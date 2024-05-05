00223c2d3cec63b17e88de499d54a286bce80493
berginiy <pirozokaltynbek@gmail.com>
2024-05-05 18:44:35 +0600

    nm,

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,14 @@
-import styles from './MainPage.module.css';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './MainPage.module.css';
 
 function MainPage() {
     const [pokemon, setPokemon] = useState([]);
     const [loading, setLoading] = useState(true);
     const [loadingMore, setLoadingMore] = useState(false);
     const [nextPage, setNextPage] = useState('https://pokeapi.co/api/v2/pokemon/?limit=20');
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         fetchData();
@@ -41,40 +42,76 @@ function MainPage() {
         fetchData();
     };
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleSearchSubmit = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        try {
+            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchQuery.toLowerCase()}`);
+            setPokemon([res.data]);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching Pokemon data:', error);
+            setPokemon([]); // Очищаем результаты поиска, если ничего не найдено
+            setLoading(false);
+        }
+    };
 
     return (
         <div className={styles.list}>
+            {/* Форма для поиска */}
+            <form onSubmit={handleSearchSubmit} className={styles.searchForm}>
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={handleSearchInputChange}
+                    placeholder="Enter Pokemon name"
+                    className={styles.searchInput}
+                />
+                <button type="submit" className={styles.searchButton}>Search</button>
+            </form>
+
+            {/* Список покемонов */}
             <ul className={styles.cards}>
-                {pokemon.map((item, index) => (
-                    <li key={index} className={styles.card}>
-                        <div className={styles.cardHover}>
-                            <div className={styles.cardHoverContent}>
-                                <h3 className={styles.cardHoverTitle}>
-                                    {item.name}
-                                </h3>
-                                <p className={styles.cardHoverText}>Weight: {item.weight}</p>
-                                <p className={styles.cardHoverText}>Type: {item.types.map(type => 
-                                    <span key={type.type.name} className={styles.type}>{type.type.name}</span>
-                                )}</p>
-                                <Link to={'/pokemon/' + item.id} className={styles.cardHoverLink}>
-                                    <span>Learn More</span>
-                                    <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                                    </svg>
-                                </Link>
+                {loading ? (
+                    <div>Loading...</div>
+                ) : pokemon.length > 0 ? (
+                    pokemon.map((item, index) => (
+                        <li key={index} className={styles.card}>
+                            {/* Карточка покемона */}
+                            <div className={styles.cardHover}>
+                                <div className={styles.cardHoverContent}>
+                                    <h3 className={styles.cardHoverTitle}>
+                                        {item.name}
+                                    </h3>
+                                    <p className={styles.cardHoverText}>Weight: {item.weight}</p>
+                                    <p className={styles.cardHoverText}>Type: {item.types.map(type =>
+                                        <span key={type.type.name} className={styles.type}>{type.type.name}</span>
+                                    )}</p>
+                                    <Link to={'/pokemon/' + item.id} className={styles.cardHoverLink}>
+                                        <span>Learn More</span>
+                                        <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                                <div className={styles.cardHoverExtra}>
+                                    <h4>Additional Info</h4>
+                                    <p>Put additional information here.</p>
+                                </div>
+                                <img src={item.sprites.other['official-artwork'].front_default} alt="" className={styles.cardHoverImage} />
                             </div>
-                            <div className={styles.cardHoverExtra}>
-                                <h4>Additional Info</h4>
-                                <p>Put additional information here.</p>
-                            </div>
-                            <img src={item.sprites.other['official-artwork'].front_default} alt="" className={styles.cardHoverImage} />
-                        </div>
-                    </li>
-                ))}
+                        </li>
+                    ))
+                ) : (
+                    <div>No results found</div>
+                )}
             </ul>
+
+            {/* Кнопка "Загрузить еще" */}
             {nextPage && (
                 <button onClick={handleLoadMore} className={styles.loadMoreButton}>
                     Load More
